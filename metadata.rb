44173ea401842a58c02e34ea08e2d1eaf7d1afc8
Jeff Gillis <jgillis@optoro.com>
2014-07-25 14:45:13 -0400

    DO-190: Update rabbitmq cookbook version.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jgillis@optoro.com'
 license          'All rights reserved'
 description      'Installs/Configures optoro_rabbitmq'
 long_description 'Installs/Configures optoro_rabbitmq'
-version          '0.1.4'
+version          '0.1.5'
 
 depends "monit"
 depends "rabbitmq"
